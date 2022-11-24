724e46dbc58336c680441d4f92c5b9477f14fed2
paulopereira98 <57216738+paulopereira98@users.noreply.github.com>
2022-11-24 13:24:52 +0000

    sync utility: add arg parser

--- scripts/sync.py
+++ scripts/sync.py
@@ -11,20 +11,25 @@ import argparse
 NSEC_IN_SEC = 1000000000
 AUX_VID_PREFIX = "sync"
 
-def thread_camera(indexes :list):
+def thread_camera(fps :float, dec: int, camIDs :list, indexes :list, time :int):
 
-    cmd = '../vimbaperf/vimbaperf --stealth --frameRate=10 --time=15 --decimation=2 '
+    cmd = f'../vimbaperf/vimbaperf --stealth --frameRate={fps} --time={time} --decimation={dec} '
+
+    for id in camIDs:
+        cmd += f'--cameraID={id} '
+
     for index in indexes:
         cmd += f'--index={index} '
+
     cmd += f'--outputFile=tmp/{AUX_VID_PREFIX}'
     print(cmd)
     os.system(cmd)
 
 
 
-def thread_audio():
+def thread_audio(interface :str, time:int):
 
-    cmd = f'sudo ../avbperf/avbperf -ienp4s0f1 -a default -t15 -ftmp/sync_mic'
+    cmd = f'sudo ../avbperf/avbperf --ifname={interface} -a default --timeout={time} -ftmp/sync_mic'
     sleep(4)
     os.system(cmd)
 
@@ -58,20 +63,43 @@ class MediaFile:
         self.filename = self.filename.replace(AUX_VID_PREFIX+'_', '')
 
 
+def setup_parser():
+    parser = argparse.ArgumentParser(description='Sync utility.')
+    parser.add_argument('-f', "--frameRate", default=10, type=float, metavar='FPS', 
+                        help='Target framerate (default 10 fps)')
+    parser.add_argument('-d', "--decimation", default=2, type=int, metavar='FACTOR', 
+                        help='On-camera image decimation factor. between 1 and 8 (default = 2)')
+    parser.add_argument('-c', "--cameraID", default=[], type=str, metavar='ID', action='append',
+                        help='Camera id. More than one can be used (ex: -c id0 -c id1)')
+    parser.add_argument('-i', "--index", default=[], type=int, metavar='#', action='append',
+                        help='Camera index (default 0). More than one can be used (ex: -i0 -i1)')
+    parser.add_argument('-t', "--time", default=0, type=int, metavar='s',
+                        help='Duration in time (default = 0 freerun)')
+    parser.add_argument('-o', "--outputFile", default="", type=str, metavar='name',
+                        help='save stream to video file (name_id.mp4)')
+    parser.add_argument('-I', "--ifname", default="", type=str, metavar='ifname',
+                        help='Audio AVB Network Interface')
+    return parser
 
 
-def main(argv, argc):
+def main():
+    parser = setup_parser()
+    args = parser.parse_args()
 
+    if args.ifname == '':
+        print("Audio AVB network interface is required")
+        exit(0)
 
-    
     try:
         os.mkdir("tmp/")
     except FileExistsError:
         pass
 
     
-    cam_p = Process(target=thread_camera, args=([0,1],))
-    mic_p = Process(target=thread_audio)
+    cam_p = Process(target=thread_camera, 
+                    args=(args.frameRate, args.decimation, args.cameraID, args.index, args.time,))
+    mic_p = Process(target=thread_audio, 
+                    args=(args.ifname, int(args.time),))
 
     cam_p.start()
     mic_p.start()
@@ -117,7 +145,11 @@ def main(argv, argc):
 
         cmd += f'-map 0:v:0 -map 1:a:0 -c:a aac -c:v copy -shortest '
         cmd += f'-metadata creation_time="{latest_stamp}" '
+
+        if args.outputFile != '':
+            vfile.filename = args.outputFile + '_' + vfile.filename
         cmd += f'"./{vfile.filename}.mp4"'
+
         print(cmd)
         os.system(cmd)
 
