c19e693e66911a1aea9b2575c7edbaf306fbf7ff
paulopereira98 <57216738+paulopereira98@users.noreply.github.com>
2022-12-05 15:16:46 +0000

    Update end_dev_vlan_config.sh

--- scripts/end_dev_vlan_config.sh
+++ scripts/end_dev_vlan_config.sh
@@ -11,8 +11,10 @@ else
     cmd="sudo ip link add link $1 \
                 name vlan0 type vlan id 0 \
                 egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7"
-    
     echo $cmd
+    eval $cmd
 
+    cmd="sudo ip link set vlan0 up"
+    echo $cmd
     eval $cmd
 fi
