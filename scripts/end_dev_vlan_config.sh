a6f27161fc5986ee573c84fe26b841112d2fdd28
paulopereira98 <57216738+paulopereira98@users.noreply.github.com>
2022-12-05 15:09:23 +0000

    scripts: add vlan config script

---
+++ scripts/end_dev_vlan_config.sh
@@ -0,0 +1,18 @@
+
+
+
+if [ -z "$1" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+then
+    echo "Configs vlan0 on given interface"
+    echo "Usage:"
+    echo "$0 ifname"
+else
+
+    cmd="sudo ip link add link $1 \
+                name vlan0 type vlan id 0 \
+                egress-qos-map 0:0 1:1 2:2 3:3 4:4 5:5 6:6 7:7"
+    
+    echo $cmd
+
+    eval $cmd
+fi
