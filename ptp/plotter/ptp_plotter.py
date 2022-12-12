c3a99b5eb358e26d713004d9c836c7faec42b54d
paulopereira98 <57216738+paulopereira98@users.noreply.github.com>
2022-12-12 19:02:35 +0000

    ptp: added plotter utility

---
+++ ptp/plotter/ptp_plotter.py
@@ -0,0 +1,67 @@
+
+# https://www.geeksforgeeks.org/plot-multiple-lines-in-matplotlib/
+# Parser script to plot PTP delay
+#
+# Vincent Jordan
+# 2020.10.12
+#
+# Paulo Pereira
+# 2022.12.13
+#   -Changed regex pattern to match current ptp4l desktop implementation
+#   -Added matplotlib plotting
+#
+# Run with:
+# ptp4l options -m -s > file
+# cat file | grep "master offset" | python3 ptp_plotter.py
+ 
+import re
+import fileinput
+import sys
+import matplotlib.pyplot as plt
+
+
+minKernelTime = 0
+maxKernelTime = 1000000000
+pattern = 'ptp4l\[(.+)\]: master offset\s+(-?[0-9]+) s([012]) freq\s+([+-]\d+) path delay\s+(-?\d+)$'
+test_string = 'ptp4l[17498.733]: master offset    2551157 s0 freq -127500 path delay    481272'
+
+kernelTime_arr = []
+masterOffset_arr = []
+freq_arr = []
+pathDelay_arr = []
+
+# Gnuplot data header
+print('# time, offset, freq, pathDelay')
+
+for line in fileinput.input():
+    # Regex search
+    res = re.search(pattern, line)# if pattern was matched
+    if res:
+        # Capture result
+        kernelTime   = res.group(1)
+        masterOffset = res.group(2)
+        state        = res.group(3)
+        freq         = res.group(4)
+        pathDelay    = res.group(5)    
+        if (state == '2') :#and (float(kernelTime) > minKernelTime) and (float(kernelTime) < maxKernelTime):
+            #print(kernelTime, masterOffset, freq, pathDelay)
+            kernelTime_arr  .append(float(kernelTime))
+            masterOffset_arr.append(int(masterOffset))
+            freq_arr        .append(int(freq))
+            pathDelay_arr   .append(int(pathDelay))
+            #if (len(pathDelay_arr) > 100):
+            #    break
+
+        
+    # if issue in patter
+    else:
+        print("Regex error:", line)
+        sys.exit()
+
+
+
+plt.plot(kernelTime_arr, masterOffset_arr,  label = 'Offset',       linestyle="-")
+plt.plot(kernelTime_arr, freq_arr,          label = 'Frequency',    linestyle="--")
+plt.plot(kernelTime_arr, pathDelay_arr,     label = 'Path Delay',   linestyle="-.")
+plt.legend()
+plt.show()
